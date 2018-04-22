4a38079c534a98cd8d6cb8a582f6775e887f0207
semux <admin@semux.org>
2018-04-22 11:47:15 -0400

    Add cmake configuration

---
+++ src/main/native/org_semux_evmc_jni_Native.cpp
@@ -0,0 +1,30 @@
+#include "org_semux_evmc_jni_Native.h"
+
+JNIEXPORT jbyteArray JNICALL Java_org_semux_evmc_jni_Native_create_1evmjit
+(JNIEnv *env, jclass cls)
+{
+    return nullptr;
+}
+
+JNIEXPORT jbyteArray JNICALL Java_org_semux_evmc_jni_Native_create_1example_1evm
+(JNIEnv *env, jclass cls)
+{
+    return nullptr;
+}
+
+JNIEXPORT void JNICALL Java_org_semux_evmc_jni_Native_set_1option
+(JNIEnv *env, jclass cls, jlong, jbyteArray, jbyteArray)
+{
+}
+
+JNIEXPORT jbyteArray JNICALL Java_org_semux_evmc_jni_Native_execute
+(JNIEnv *env, jclass cls, jobject, jint, jbyteArray, jbyteArray)
+{
+    return nullptr;
+}
+
+JNIEXPORT void JNICALL Java_org_semux_evmc_jni_Native_destroy
+(JNIEnv *env, jclass cls, jlong)
+{
+
+}

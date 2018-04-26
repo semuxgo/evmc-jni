00f6d11cf4ad6c25b6d16e940378bdd00cf3487a
semux <admin@semux.org>
2018-04-25 22:32:31 -0400

    Update status code

--- src/main/java/org/semux/evmc/jni/type/StatusCode.java
+++ src/main/java/org/semux/evmc/jni/type/StatusCode.java
@@ -18,63 +18,78 @@ public enum StatusCode {
      */
     FAILURE(1),
 
-    OUT_OF_GAS(2),
-
-    /**
-     * Unknown instruction encountered by the Evm.
-     */
-    UNDEFINED_INSTRUCTION(3),
-
-    BAD_JUMP_DESTINATION(4),
-
-    STACK_OVERFLOW(5),
-
-    STACK_UNDERFLOW(6),
-
     /**
      * Execution terminated with REVERT opcode.
      */
-    REVERT(7),
+    REVERT(2),
+
+    /**
+     * The execution has run out of gas.
+     */
+    OUT_OF_GAS(3),
+
+    /**
+     * The designated INVALID instruction has been hit during execution.
+     */
+    INVALID_INSTRUCTION(4),
+
+    /**
+     * An undefined instruction has been encountered.
+     */
+    UNDEFINED_INSTRUCTION(5),
+
+    /**
+     * The execution has attempted to put more items on the EVM stack
+     * than the specified limit.
+     */
+    STACK_OVERFLOW(6),
+
+    /**
+     * Execution of an opcode has required more items on the EVM stack.
+     */
+    STACK_UNDERFLOW(7),
+
+    /**
+     * Execution has violated the jump destination restrictions.
+     */
+    BAD_JUMP_DESTINATION(8),
+
+    /**
+     * Tried to read outside memory bounds.
+     */
+    INVALID_MEMORY_ACCESS(9),
+
+    /**
+     * Call depth has exceeded the limit (if any)
+     */
+    CALL_DEPTH_EXCEEDED(10),
 
     /**
      * Tried to execute an operation which is restricted in static mode.
      */
-    STATIC_MODE_VIOLATION(8),
+    STATIC_MODE_VIOLATION(11),
 
     /**
-     * The dedicated INVALID instruction was hit.
+     * A call to a precompiled or system contract has ended with a failure.
      */
-    INVALID_INSTRUCTION(9),
+    PRECOMPILE_FAILURE(12),
 
     /**
-     * Tried to read outside memory bounds.
-     *
-     * An example is RETURNDATACOPY reading past the available buffer.
+     * Contract validation has failed (e.g. due to EVM 1.5 jump validity,
+     * Casper's purity checker or ewasm contract rules).
      */
-    INVALID_MEMORY_ACCESS(10),
+    CONTRACT_VALIDATION_FAILURE(13),
 
     /**
-     * Exceptions produced by precompiles/system contracts.
-     *
-     * An example: elliptic curve functions handed invalid EC points
+     * EVM implementation generic internal error.
      */
-    PRECOMPILE_FAILURE(11),
+    INTERNAL_ERROR(-1),
 
     /**
-     * The EVM rejected the execution of the given code or message.
-     *
-     * This error SHOULD be used to signal that the EVM is not able to or willing to
-     * execute the given code type or message. If an EVM returns the ::REJECTED
-     * status code, the Client MAY try to execute it in other EVM implementation.
-     * For example, the Client tries running a code in the EVM 1.5. If the code is
-     * not supported there, the execution falls back to the EVM 1.0.
+     * The execution of the given code and/or message has been rejected
+     * by the EVM implementation.
      */
-    REJECTED(-1),
-
-    /**
-     * EVM implementation internal error.
-     */
-    INTERNAL_ERROR(-2);
+    REJECTED(-2);
 
     private int code;
 
